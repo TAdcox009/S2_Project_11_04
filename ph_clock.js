5bd31e50bffbd4bb52afb350f71a0244d8d297b9
Unknown <TAdcox009@west-mec.org>
2019-02-17 20:27:30 -0700

    Final commit
    Fixed errors in code, added comments, final commit ready for review.

--- ph_clock.js
+++ ph_clock.js
@@ -12,40 +12,47 @@
 
 */
 
-// Variable named minsLeft is set to a value of 0. This variable will keep track of the minutes left.
-// Variable named secsLeft is set to a value of 15. This variable will keep track of the seconds left within each minute.
+// 9. Scroll back to the top of the file and, directly below the statement that declares the timeLeft variable, insert a command to run the countdown() function every second. Store this timed command in a variable named clockiD.
+
+
+// Variable named minsLeft is set to a value of 30. This variable will keep track of the minutes left.
+// Variable named secsLeft is set to a value of 0. This variable will keep track of the seconds left within each minute.
 // Variable named timeLeft is set to a value of the variable equal to the number of minsLeft multiplied by 60 plus the number of secsLeft.
-var minsLeft = 0;
-var secsLeft = 15;
+// runs the countdown function at an interval of 1000.
+var minsLeft = 30;
+var secsLeft = 0;
 var timeLeft = minsLeft * 60 + secsLeft;
+var clockId = setInterval("countdown()", 1000);
 
 // The countdown function will be used to update the minsLeft, secsLeft, and timeLeft variables every second.
 // A new value is going to be set for the minsLeft variable by dividing the timeLeft variable by 60, using the Math.floor() method.
 // A new value for the secsLeft variable is equal to the value of the timeLeft variable minus 60 times the minsLeft variable.
-function countdown() {
+//When the minsLeft or secsLeft values are between 0 and 9, a new variable named minsstring is set equal to the value returned by the addLeadingZero() function using minsLeft as the parameter value.
+function countdown(){
     minsLeft = Math.floor(timeLeft / 60);
     secsLeft = timeLeft - 60 * minsLeft;
     var minsString = addLeadingZero(minsLeft);
-    var secsString = addLeadingZero(secsString);
+    var secsString = addLeadingZero(secsLeft);
+
+    // In the element with #minutes the text content is going to change to the value of the minsString variable.
+    // In the element with #seconds the text content is going to change to the value of the secsString variable.
+    document.getElementById("minutes").textContent = minsString;
+    document.getElementById("seconds").textContent = secsString;
+
+    //This is calling the function checkTimer() and it has no parameters.
+    checkTimer();
+
+    //This is going to decreasethe value of the timeLeft variable by 1.
+ timeLeft --;
 }
-
-
-
-// c. Randall wants the countdown clock to display leading zeroes when the minsLeft or secsLeft values are between 0 and 9. The addLeadingZero() function has been provided for you to add these zeroes if necessary. Create a new variable named minsstring and set it equal to the value returned by the addLeadingZero() function using minsLeft as the parameter value.
-
-// d. Call the addLeadingZero() function again using secsLeft as the parameter value and store the result in the secsstring variable.
-
-// e. Within the element with the ID minutes, change text content to the value of the minsString variable. Within the element with the ID seconds, change the text content to the value of the secsString variable.
-
-// f. Randall has supplied you with a function named checkTimer()that will check whether there is any time left to submit the order. Add a command to call this function. The function has no parameters.
-
-// g. Use the decrement operator to decrease the value of the timeLeft variable by 1.
-
-// 9. Scroll back to the top of the file and, directly below the statement that declares the timeLeft variable, insert a command to run the countdown() function every second. Store this timed command in a variable named clockiD.
-
-
-
-
+   
+// function named stopclock() the purpose is to stop the clock when the time has run out and to notify the user that the time has expired, this function has no parameters.
+//  the insertAdjacentHTML() method is being used to insert the following HTML code before the end of the page element with the ID TimeHead.
+// Use the clearInterval() method to clear the timed command stored in the clockID and stop it from continuing to run.
+function stopClock() {
+    document.getElementById("TimeHead").insertAdjacentHTML('beforeend', "<br />(Order Expired)" );
+    clearInterval(clockId);
+}
 
 
 
